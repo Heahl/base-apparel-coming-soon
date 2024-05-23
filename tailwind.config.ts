3c7ee6dd2acc295f88bd9a00773774d387b83979
heahl <hendrik712.a@gmail.com>
2024-05-23 15:56:39 +0200

    final commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,26 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors : {
+      'desaturated-red': 'hsl(0, 36%, 70%)',
+      'soft-red': 'hsl(0, 93%, 68%)',
+      'dark-grayish-red': 'hsl(0, 6%, 24%)',
+      'gradient-start': 'hsl(0, 80%, 86%)',
+      'gradient-end': 'hsl(0, 74%, 74%)',
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        josefin: ["Jorefin Sans", ...fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      backgroundClip: ['responsive'],
+      boxDecorationBreak: ['responsive'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 } satisfies Config;
